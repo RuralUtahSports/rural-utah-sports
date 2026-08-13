8295a4bfc03bbd57998b7e6a85634d77d6a65bc7
RuralUtahSports <60403147+RuralUtahSports@users.noreply.github.com>
2026-08-12 22:36:21 -0600

    Build team tabs foundation

--- team-tabs.js
+++ team-tabs.js
@@ -1,1 +1,7 @@
-(()=>{'use strict';})();
+(()=>{
+'use strict';
+if((location.pathname.split('/').pop()||'').toLowerCase()!=='team.html')return;
+const tabs=[['overview','Overview'],['seasons','Seasons'],['playoffs','Playoffs'],['games','Games'],['elo','ELO'],['history','History']];
+function start(){console.log('RUS team tabs ready',tabs.length)}
+if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',start);else start();
+})();
